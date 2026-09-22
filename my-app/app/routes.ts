c907e1793fc1fb5bac77251c2e000747b676dbaa
Ishan Sharma <ishan.spain@gmail.com>
2026-09-22 17:17:28 +0530

    feat: add AuthLayout component and update routes for authentication

--- my-app/app/routes.ts
+++ my-app/app/routes.ts
@@ -1,6 +1,10 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, layout } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
   route("about", "routes/about.tsx"),
+   layout("./auth/layout.tsx", [
+    route("login", "./auth/login.tsx"),
+    route("register", "./auth/register.tsx"),
+  ]),
 ] satisfies RouteConfig;
